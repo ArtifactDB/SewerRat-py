081e70568c30cda1168e7bad1638be47db435e48
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-04-24 18:05:21 +0000

    Deploying to gh-pages from @ ArtifactDB/SewerRat-py@f6297343456219b305354a10f77687ddc5cf2a9c 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+gc885b16',
+    VERSION: '0.0.post1.dev1+gf629734',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
